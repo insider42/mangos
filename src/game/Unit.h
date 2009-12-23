d9a19d1d3cdbe68df73932201cc13deb0a2efd2e
Insider <insider42rus@yandex.ru>
2009-12-23 17:33:48 +0700

    [test patch 34] Implementation of SPELL_AURA_IGNORE_UNIT_STATE. This fixes spells 29723,44543,56636,57499,64976 and ranks

--- src/game/Unit.h
+++ src/game/Unit.h
@@ -1137,6 +1137,7 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         bool IsPolymorphed() const;
 
         bool isFrozen() const;
+        bool isIgnoreUnitState(SpellEntry const *spell);
 
         void RemoveSpellbyDamageTaken(AuraType auraType, uint32 damage);
 
@@ -1626,6 +1627,7 @@ class MANGOS_DLL_SPEC Unit : public WorldObject
         uint32 m_reactiveTimer[MAX_REACTIVE];
         uint32 m_regenTimer;
         uint32 m_lastManaUseTimer;
+        float m_lastAuraProcRoll;
 
     private:
         void CleanupDeletedAuras();
